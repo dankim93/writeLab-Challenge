0733d2a65619bbdaa7486ecd358d4a795d5f5415
dankim93 <dan_kim93@hotmail.com>
2017-11-14 23:11:00 -0800

    Add collectData to collect data for certain year and course

--- src/components/grid.js
+++ src/components/grid.js
@@ -4,9 +4,34 @@ import {bindActionCreators} from 'redux';
 import * as actions from '../actions/data_actions';
 
 class Grid extends React.Component {
+  state = {
+    data: {}
+  }
+
+  collectGridData() {
+    // let data = {};
+    this.state.data = {};
+    for (let i = 0; i < this.props.classes.length; i++) {
+
+      if (this.state.data[this.props.classes[i].instructor] >= 0 && this.props.classes[i].course === this.props.courseName) {
+        this.state.data[this.props.classes[i].instructor] += this.props.classes[i].students;
+      } else if (this.state.data[this.props.classes[i].instructor] === undefined && this.props.classes[i].course === this.props.courseName) {
+        this.state.data[this.props.classes[i].instructor] = this.props.classes[i].students;
+      }
+
+    }
+
+    console.log(this.state.data);
+  }
+
+
+
   render() {
+    this.collectGridData();
     return(
-      <div>{this.props.courseName}</div>
+      <div>
+        {this.props.courseName}
+      </div>
     )
   }
 }
